fe2e6b6998dad120bc855cca50c557ac2a8d1c3f
Avoler0 <jys1391@gmail.com>
2022-10-23 02:38:08 +0900

    Feat: 랭크 별 정보 Card 추가

--- component/summoner/league/card.tsx
+++ component/summoner/league/card.tsx
@@ -1,21 +1,81 @@
 import styled from "styled-components";
-
+import Image from "next/image";
 
 export default function LeagueCard({info,margin}:any){
   console.log("리그 정보",info,margin);
+  const {freshBlood,hotStreakinactive,leagueId,leaguePoints,losses,queueType,rank,summonerId,summonerName,tier,veteran,wins} = info
+  const rankType = queueType === "RANKED_SOLO_5x5" ? "솔로 랭크" : "자유 랭크"
+  console.log(queueType);
   
+  const rate = (wins/(wins+losses))*100
   return(
     <Card margin={margin} >
-      
+      <RankTitle>{rankType}</RankTitle>
+      <InfoWrap>
+        <ImageDiv>
+          <div>
+            <Image src={`/images/tier-icons/tier-icons/${info.tier.toLowerCase().trim()}_${info.rank.toLowerCase().trim()}.png`} alt="tier" width="80" height="80" objectFit="cover"/>
+          </div>
+        </ImageDiv>
+        <ContentDiv>
+          <Rank>
+            <div>{tier} {rank}</div>
+            <div>{leaguePoints}LP</div>
+          </Rank>
+          <WinRate>
+            <div>
+              <span>{wins}승</span>
+              &nbsp;
+              <span>{losses}패</span>
+            </div>
+            <div>
+              <span>승률 {Math.floor(rate)}%</span>
+            </div>
+          </WinRate>
+        </ContentDiv>
+      </InfoWrap>
     </Card>
   )
 
 }
 
 const Card = styled.div<{margin:boolean}>`
+  align-items: center;
   width: 100%;
   height: 8rem;
   background-color: #2c3e50;
   border-radius: 0.5rem;
   margin-bottom: ${props => props.margin ? "0.5rem":"0"}
+`;
+const RankTitle = styled.div`
+  padding: 0.1rem 0 0.2rem 0.7rem;
+  color: white;
+  font-weight: bold;
+  border-bottom: 1px solid #706a6a;
+`;
+const InfoWrap = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const ImageDiv = styled.div`
+  flex: 4;
+  div{
+    display: inline-block;
+    padding: 0.5rem;
+    border-radius: 5rem;
+  }
+`;
+const ContentDiv = styled.div`
+  width: 100%;
+  flex: 6;
+  display: flex;
+  justify-content: space-between;
+`;
+const Rank = styled.div`
+  color: white;
+  font-size: 0.9rem;
+`;
+const WinRate = styled.div`
+  padding-right: 1rem;
+  color: #cccccc;
 `;
