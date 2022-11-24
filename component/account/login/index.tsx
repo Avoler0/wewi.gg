0c0504282dba059bdf0a4c78b80c71ce0e1972e9
Avoler0 <jys1391@gmail.com>
2022-11-24 19:13:47 +0900

    Refactor: 로그인 , 회원가입 리팩토링 준비

---
+++ component/account/login/index.tsx
@@ -0,0 +1,274 @@
+
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+// import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+// import { accountLogin } from "../../../../Redux/accountRedux/accountSlice";
+import React from "react";
+import Link from "next/link";
+// import { requestApi }  from "../../../../api/requestApi";
+const { naver } = window as any;
+
+export default function Login() {
+  const dispatch = useDispatch()
+  const emailRef = React.useRef<HTMLInputElement>(null);
+  const pwRef = React.useRef<HTMLInputElement>(null);
+  const [loginError , setLoginError] = useState(false);
+  const [loginType , setLoginType] = useState("basic");
+
+  // const onLogin = async () => {
+  //   if(emailRef.current && pwRef.current){
+  //     const email = emailRef.current.value;
+  //     const password = pwRef.current.value;
+  //     requestApi.tryLogin(email,password)
+  //     .then((_response:any)=>{
+  //       if(_response.data.length){
+  //         dispatch(accountLogin(_response.data[0]))
+  //         console.log(location)
+  //         navigate("/")
+  //       }else{
+  //         setLoginError(true);
+  //         console.log("회원 데이터 없음")
+  //       }
+  //     })
+  //     .catch((error)=>{
+  //       console.log("에러",error)
+  //     })
+  //   }
+  // }
+  const naverInit = () =>{
+      const login = new naver.LoginWithNaverId({
+      clientId: 'NR61LLLoBLU2vcfbHvDY',
+      callbackUrl: 'http://localhost:3000/login', 
+      callbackHandle:true,
+      isPopup: false, // popup 형식으로 띄울것인지 설정
+      loginButton: { 
+        color: 'green', type: 3, height: '65' 
+      }, //버튼의 스타일, 타입, 크기를 지정
+    });
+    login.init();
+  };
+  // const naverToken = location.hash.split('=')[1].split('&')[0];  
+  const postNaverToken = () => {
+   if(!location.hash && loginType !== "naver" ){
+     return;
+   }
+  
+  // axios.post('http://localhost:4000/api/login/naver' , {
+  //   token:naverToken
+  // }).then((res) => {
+  //   console.log("RES",res.data);
+  //   //이메일 확인 후 가입 안되어 있으면 가입화면으로
+  //   history('/register',{state:res.data})
+  // })
+}
+ const googleLogin = () => {
+   setLoginType("google");
+   const clientID = '625687004788-gd57fikpm0v5854djf8emrm7bgmh4drg.apps.googleusercontent.com'
+   const path = 'http://localhost:3000/login'
+   const googleOauthURL =`https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientID}`+
+    `&response_type=token&redirect_uri=${path}&scope=https://www.googleapis.com/auth/userinfo.email`;
+    window.location.assign(googleOauthURL);
+ }
+ const postGoogleToken = () => {
+   if(!window.location.hash && loginType !== "google") return;
+  // const accessToken = window.location.hash.split("=")[1].split("&")[0]
+ }
+ 
+  useEffect(() => {
+    naverInit();
+    postGoogleToken();
+  }, []);
+
+  return (
+  <>
+    <Head>
+    
+    </Head>
+    <Container>
+      <Layout>
+        <LogoWrap>
+          <Logo>wewi.gg</Logo>
+        </LogoWrap>
+        <Form onSubmit={(e) => e.preventDefault()}>
+          <InWrap>
+            <Label htmlFor="loginId">이메일</Label>
+            <Input id="loginId" type="text" ref={emailRef} />
+          </InWrap>
+          <InWrap>
+            <Label htmlFor="loginPw">비밀번호</Label>
+            <Input id="loginPw" type="password" ref={pwRef} />
+          </InWrap>
+          <InWrap style={{
+            display:'flex'
+          }}>
+            <CheckBox type="checkbox" id="keepLogin"/>
+            <Label>자동 로그인</Label>
+            <IDPW>ID/PW 찾기</IDPW>
+          </InWrap>
+            {loginError &&  
+              <>
+                <ErrorMsg>아이디 또는 비밀번호를 잘못 입력했습니다.</ErrorMsg>
+                <ErrorMsg>입력하신 내용을 다시 확인해주세요.</ErrorMsg>
+              </>
+            }
+          <OR>OR</OR>
+          <FastLogin>간편 로그인</FastLogin>
+          <NaverLogin id='naverIdLogin' onClick={postNaverToken}>
+              네이버 로그인
+          </NaverLogin>
+          <GoogleLogin onClick={googleLogin}>
+            {/* <img src="../images/path-icons/btn_google_signin_dark_normal_web@2x.png" style={{
+              width:"314px",
+              borderRadius:"20px",
+              cursor: "pointer",
+            }} /> */}
+          </GoogleLogin>
+          <ExitWrap>
+            <OkBt>로그인</OkBt>
+            
+          </ExitWrap>
+          <Register>
+            <span>아이디가 없다면 ?</span>
+            <Link to="/register">회원가입</Link>
+          </Register>
+        </Form>
+        
+      </Layout>
+    </Container>
+  </>
+  );
+}
+
+const Container = styled.div`
+  width: 450px ;
+  min-height: 682px;
+  margin: 0 auto ;
+  height: 25rem;
+  background-color: #2c3e50;
+`;
+const Layout = styled.div`
+  margin: 0 40px;
+  
+`;
+const Head = styled.div`
+  height: 13rem ;
+  padding-bottom: 48px;
+`;
+const LogoWrap = styled.div`
+  margin: 0 auto;
+`;
+const Logo = styled.h1`
+  font-size: 48px;
+  font-weight: bold;
+  color: white;
+  text-align: center;
+  padding: 32px 0 24px 0;
+`;
+const Form = styled.form`
+  display:flex ;
+  flex-direction:column ;
+  margin: auto ;
+  padding: 1rem;
+  padding-top: 0;
+`;
+const InWrap = styled.div`
+  margin-top: 1rem;
+`;
+const Label = styled.label`
+  font-size: 15px;
+  display: block;
+  /* color: rgba(123,122,142,0.8); */
+  color: white;
+  margin-bottom: 8px;
+`;
+const Input = styled.input`
+  width: 100%;
+  height: 2.5rem;
+  font-size: 0.875rem;
+  background-color: #28283b;
+  color: white;
+  border: 1px solid rgba(0,0,0,0.2);
+  border-radius: 5px;
+  padding: 0.8rem;
+  :focus,:active{
+    outline: none;
+  }
+`;
+const CheckBox = styled.input`
+  
+`;
+const IDPW = styled.div`
+  font-size: 15px;
+  color: #8686f0;
+  margin: 0 10px;
+  cursor: pointer;
+`;
+const OR = styled.div`
+  display: flex;
+  flex-basis: 100%;
+  align-items: center;
+  margin: 8px 0px;
+  font-size: 14px;
+  text-align: center;
+  ::before,::after{
+  content: "";
+  flex-grow: 1;
+  background: rgba(0, 0, 0, 0.35);
+  height: 1px; font-size: 0px;
+  line-height: 0px;
+  margin: 0px 16px;
+  }
+  
+`;
+const FastLogin = styled.h2`
+
+`;
+const NaverLogin = styled.div`
+  text-align:center;
+  padding: 10px;
+`;
+const NaverLink = styled.div`
+  display: inline;
+`;
+const GoogleLogin = styled.div`
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  margin: 8px 0;
+  font-size: 18px;
+  text-align: center;
+`;
+const ExitWrap = styled.div`
+  margin-top: 30px;
+  display: flex;
+  justify-content: space-between;
+  text-align: center;
+`;
+const OkBt = styled.button`
+  margin: 0 auto;
+  width: 303px;
+  height: 50px;
+  border: 0.08rem solid rgba(123,122,142,0.8);
+  border-radius: 5px;
+  background-color: #b5b5c0;
+  color: #fff;
+  cursor: pointer;
+  font-size: 22px;
+`;
+const Register = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 1rem;
+  padding: 0 2.5rem 0 2.5rem;
+  text-align: center;
+  a{
+    text-decoration: underline;
+    color:#1ea1f7;
+  }
+`;
+const ErrorMsg = styled.div`
+  padding-left: 5px;
+  font-size: 12px;
+  color: #ff0000c5;
+`;
---
+++ component/account/register/index.tsx
@@ -0,0 +1,241 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+
+
+
+
+function Register() {
+  // const pwPattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\d~!@#$%^&*()+|=]{8,20}$/;
+  // const [validation,setValidation] = useState(
+  //   {email:false,
+  //   password:false,
+  //   nickName:false,
+  //   readEmail:false}
+  // )
+  // const [value,setValue] = useState(
+  //   {email:"",
+  //   password:"",
+  //   nickName:"",
+  //   readEmail:""}
+  // )
+  // const history = useNavigate();
+  // const toolTip:any = {
+  //   email:["올바른 이메일 형식이 아닙니다." , "이메일을 다시 한번 입력 해 주세요."],
+  //   password:["비밀번호가 올바르지 않습니다." , "8~20자 영문 대소문자, 숫자, 특수문자를 이용하여 완성해주세요."],
+  //   nickName:["존재하지 않는 닉네임입니다." , "다시 한번 확인해주세요."],
+  //   readEmail:["이미 등록된 이메일입니다."]
+  // }
+  // async function checkValidation(){
+  //   let valid = validation;
+  //   valid.email = value.email.includes("@") && value.email.includes(".")
+  //   valid.password = pwPattern.test(value.password)
+  //   valid.readEmail = await overlapEmail()
+  //   valid.nickName = await checkNickname()
+    
+  //   setValidation({...valid})
+  // }
+  // function overlapEmail(){
+  //   const result = requestApi.readEmail(value.email)
+  //     .then((_response:any)=>{
+  //       if(_response.data.length === 0){
+  //         return true
+  //       }else{
+  //         return false;
+  //       }
+  //     })
+  //     return result;
+  // }
+  // function checkNickname(){
+  //   const result = getSummoner(value.nickName)
+  //     .then((_response)=>{
+  //       return true
+  //     })
+  //     .catch(()=>{
+  //       return false
+  //     })
+  //     return result;
+  // }
+  // function postRegister() {
+  //   requestApi.saveRegister({type:"general", ...value})
+  // }
+
+  // function onSubmit(event:any){
+  //   let valueData = value;
+  //   valueData.email = event.target[0].value;
+  //   valueData.password = event.target[1].value;
+  //   valueData.nickName = event.target[2].value;
+  //   setValue({...valueData})
+
+  //   checkValidation()
+    
+    
+  //   event.preventDefault();
+  // }
+  // useEffect(()=>{
+  //   const falseValid = Object.entries(validation).find((data:any) => {
+  //     if(data[1] === false) return true
+  //   })
+  //   if(!falseValid) postRegister();
+  // },[validation])
+  // function ValidationToolTip(){
+  //     const falseValid = Object.entries(validation).find((data:any) => {
+  //       if(data[1].valid === false) return true
+  //     })
+  //     return (
+  //       <Tooltip>
+  //             {
+  //             falseValid &&
+  //             <>
+  //               <span>{toolTip[falseValid[0]]}</span>
+  //               <br/>
+  //             </> 
+  //             }
+  //       </Tooltip>
+  //     )
+  // }
+  return (
+    <>
+    <Head />
+    <Container>
+      <Layout>
+        <LogoWrap>
+          <Logo>wewi.gg</Logo>
+        </LogoWrap>
+        <SignTitle>기본 정보 입력</SignTitle>
+        <SignExp>회원가입을 위해서 이메일 인증이 진행되며, 인증이 완료되기 전까지 회원가입이 완료가 되지 않습니다.</SignExp>
+        <Form onSubmit={(e) => onSubmit(e)}>
+          <InputDiv>
+            <Label htmlFor="regiId">이메일 주소</Label>
+            <Input type="text" name="email" />
+          </InputDiv>
+          <InputDiv>
+            <Label>비밀번호</Label>
+            <Input type="password" name="password" />
+          </InputDiv>
+          <InputDiv>
+              <Label>닉네임</Label>
+              <NickSub >자신의 롤 닉네임을 입력 해 주세요</NickSub>
+              <Input type="text" name="nickName" />
+          </InputDiv>
+          <ValidationToolTip />
+          <ExitWrap>
+            <CancelBt onClick={() => history('/')}>취소</CancelBt>
+            <OkBt>가입하기</OkBt>
+          </ExitWrap>
+        </Form>
+      </Layout>
+      
+    </Container>
+    </>
+  )
+}
+
+export default Register;
+
+const Container = styled.div`
+  width: 610px ;
+  min-height: 100%;
+  margin: 0 auto ;
+  background-color: #2c3e50;
+`;
+const Layout = styled.div`
+  margin: 0 60px;
+  padding: 1rem;
+`;
+const Head = styled.div`
+  height: 13rem ;
+  padding-bottom: 48px;
+`;
+const LogoWrap = styled.div`
+  margin: 0 auto;
+`;
+const Logo = styled.h1`
+  font-size: 48px;
+  font-weight: bold;
+  color: white;
+  text-align: center;
+  padding: 32px 0 24px 0;
+`;
+const SignTitle = styled.div`
+  font-size: 24px;
+`;
+const SignExp = styled.div`
+  padding: 10px;
+  border: 1px solid #0a82a0;
+  border-radius: 10px;
+  margin: 10px 0;
+`;
+const Form = styled.form`
+  display:flex ;
+  flex-direction:column ;
+  margin: auto ;
+  
+  padding-top: 0;
+`;
+const InputDiv = styled.div`
+  position: relative;
+  margin-top: 1rem;
+`;
+const Label = styled.label`
+  font-size: 15px;
+  display: inline-block;
+  /* color: rgba(123,122,142,0.8); */
+  color: white;
+  margin-bottom: 8px;
+`;
+const Input = styled.input`
+  width: 100%;
+  height: 2.5rem;
+  font-size: 0.875rem;
+  /* background-color: #28283b; */
+  background-color: transparent;
+  color: white;
+  /* border: 1px solid rgba(0,0,0,0.2); */
+  border: none;
+  border-bottom: 1px solid #f3ebeb33;
+  border-radius: 5px;
+  :focus,:active{
+    outline: none;
+  }
+`;
+const ExitWrap = styled.div`
+  margin: 2.5rem 0;
+  display: flex;
+  justify-content: space-between;
+`;
+const CancelBt = styled.button`
+  width: 42%;
+  height: 3.2rem;
+  border: 0.08rem solid rgba(123,122,142,0.8);
+  border-radius: 5px;
+  background-color: transparent;
+  cursor: pointer;
+  font-size: 22px;
+`;
+const OkBt = styled.button`
+  width: 42%;
+  height: 3.2rem;
+  border: 0.08rem solid rgba(123,122,142,0.8);
+  border-radius: 5px;
+  background-color: #7c7c83;
+  cursor: pointer;
+  font-size: 22px;
+`;
+const NickSub = styled.span`
+  position: absolute;
+  right: 0;
+  display: inline-block;
+  margin: 0 10px;
+  font-size: 12px;
+  line-height: 18px;
+  color: #cf9797cc;
+`;
+const Tooltip = styled.div`
+  margin-top: 0.7rem;
+  right:0;
+  span{
+    font-size: 12px;
+    color: #ca9090;
+  }
+`;
