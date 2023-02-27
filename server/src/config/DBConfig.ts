76e6b287d54966504d87b7171eba90f8f33ca16c
Avoler0 <jys1391@gmail.com>
2023-02-27 18:37:32 +0900

    Feat: DBConfig env

--- server/src/config/DBConfig.ts
+++ server/src/config/DBConfig.ts
@@ -2,8 +2,6 @@ export const config = {
     "host": process.env.HOST,
     "user": process.env.SQL_ID,
     "password": process.env.SQL_PW,
-    // "user": "root",
-    // "password": "123",
     "database": "wewigg",
     "checkDuplicate": false,
     "connectionLimit": 100,
