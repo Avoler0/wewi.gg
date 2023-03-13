1e73c0811f9b539de57c384ace122890407a12d8
Avoler0 <jys1391@gmail.com>
2023-03-13 17:01:37 +0900

    Fix: 빌드에러 수정

--- client/component/gnb/header/header.tsx
+++ client/component/gnb/header/header.tsx
@@ -37,7 +37,7 @@ export default function HeaderJSX(){
             </Login>
           )}
         </UserWrap>
-        {showMenu && <UserMenu show={setShowMenu} />}
+        {/* {showMenu && <UserMenu show={setShowMenu} />} */}
       </Content>
       
     </Header>
